cdf2e19070ffd3cfc53fec1ed65912ac284d8d7c
Seohana <onnuri1226@gmail.com>
2026-02-19 17:15:05 +0900

    docs : ReadMe 작성 및 비즈니스 로직, ERD 이미지 추가

--- src/main/java/com/bootcamp/paymentdemo/domain/product/dto/SearchProductResponse.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.paymentdemo.domain.product.dto;
-// TODO 결제 테스트용, 삭제 필요
-public record SearchProductResponse(
-        String id
-        , String name
-        , Long price
-        , Long stock
-) {
-}
